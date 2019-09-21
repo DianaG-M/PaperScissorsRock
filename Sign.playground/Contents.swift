559297b013c1bcc94627286aa7a3de9ae344a285
Apple Device 24 <appledevice24@Apple-Device-24.local>
2019-09-21 13:27:45 -0500

    bajando cambios

--- Sign.playground/Contents.swift
+++ Sign.playground/Contents.swift
@@ -1,15 +1,7 @@
 import UIKit
 import GameplayKit
 
+enum Sig{
+    
+}
 
-//Subiendo primer cambio locas forever. Primera llamada... desde ramalors
-
-//Subiendo al master Fab  holi g
-
-var str = "Hello, playground"
-
-
-//Hola aquí gaby
-
-
-//Comentario de Diana
